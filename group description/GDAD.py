1d33820388207a06ad5e6b054972f6b8d156ada7
Ixarro <43796494+rttyui@users.noreply.github.com>
2022-11-01 17:45:11 +0500

    update conn
    update conn

--- group description/GDAD.py
+++ group description/GDAD.py
@@ -5,13 +5,19 @@ from ldap3 import Server, Connection, SIMPLE, SYNC, ASYNC, SUBTREE, ALL
 AD_SERVER = 'corp.ertelecom.loc'
 # Пользователь (логин) в Active Directory - нужно указать логин в AD
 # в формате 'CORP\aduser' или 'aduser@ertelecom.loc'
-AD_USER = input('Введите логин через corp\: ')
+domen = str('@corp.ertelecom.loc')
+login = input('Введите логин: ')
+AD_USER = (login+domen)
+print(AD_USER)
 AD_PASSWORD = input('Введите пароль: ')
-AD_SEARCH_TREE = 'dc=ertelecom,dc=loc'
+AD_SEARCH_TREE = 'dc=corp,dc=ertelecom,dc=loc'
 
 server = Server(AD_SERVER)
 conn = Connection(server,user=AD_USER,password=AD_PASSWORD)
 conn.bind()
+print(conn)
+
+
 # в ответ должно быть - True
 
 # Поиск в Active Directory
@@ -23,23 +29,21 @@ conn.bind()
 # - и вывожу атрибуты - attributes
 # Все возможные атрибуты Active Directory можно посмотреть здесь -
 # https://msdn.microsoft.com/en-us/library/ms675090%28v=vs.85%29.aspx
-conn.search(AD_SEARCH_TREE,'(&(objectCategory=Person)(!(UserAccountControl:1.2.840.113556.1.4.803:=2))(givenName=*)(sn=*))',
-    SUBTREE,
-    attributes =['cn','proxyAddresses','department','sAMAccountName', 'displayName', 'telephoneNumber', 'ipPhone', 'streetAddress',
-    'title','manager','objectGUID','company','lastLogon']
-    )
+#a = input("Введи логин для поиска: ")
+conn.search('dc=corp,dc=ertelecom,dc=loc,ou=uk,ou=users', '(objectclass=person)', attributes=['sn','givenName'])
+
 # после этого запроса в ответ должно быть - True
 
 # можно посмотреть на результат
 print(conn.entries)
 # или вывести только Common-Name - cn
-for entry in conn.entries:
-    print(entry.cn)
+#for entry in conn.entries:
+ #   print(entry.cn)
 
 # Найти пользователя с логином admin (sAMAccountName=admin) и показать информацию по нему
-conn.search(AD_SEARCH_TREE,'(&(objectCategory=Person)(sAMAccountName=admin))', SUBTREE,
-    attributes =['cn','proxyAddresses','department','sAMAccountName', 'displayName', 'telephoneNumber', 'ipPhone', 'streetAddress',
-    'title','manager','objectGUID','company','lastLogon']
-    )
+#slogin = input("введите логин для поиска: ")
 
-conn.entries
+#conn.search(AD_SEARCH_TREE,'(sAMAccountName=805306368)', SUBTREE,
+ #  attributes =['cn','sAMAccountName','gn']
+  #  )
+#conn.entries
